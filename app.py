1c51f9cfaccb62b9a5e8767ddc0ce219cc3df71e
Balsa-Dogandzic <100625564+Balsa-Dogandzic@users.noreply.github.com>
2023-05-21 17:33:36 +0200

    Templating

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html")
+
+@app.route("/books")
+def books():
+    return render_template("books.html")
+
+@app.route("/books/<book>/")
+def single_book(book, name=None):
+    return render_template("book.html",book=book)
